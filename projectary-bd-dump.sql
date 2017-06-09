4517806e286c6b1c2fef42d2780cdb3b903715f3
palfaiate <syl.paulo.alfaiate@gmail.com>
2017-06-09 19:13:13 +0100

    Updated Procedures

--- projectary-bd-dump.sql
+++ projectary-bd-dump.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `projectary-master` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-USE `projectary-master`;
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.55, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: projectary-master
 -- ------------------------------------------------------
--- Server version	5.7.11-log
+-- Server version	5.5.55-0+deb8u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +14,14 @@ USE `projectary-master`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `projectary-master`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `projectary-master` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `projectary-master`;
 
 --
 -- Table structure for table `application`
@@ -571,7 +577,7 @@ BEGIN
     CALL isAdmin(userid, @isAdmin);
     IF (@isAdmin = TRUE) THEN
 		UPDATE project p SET p.finishedin = NOW(), p.finished = TRUE WHERE p.id = projectid;
-    END IF;    
+    END IF;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -923,7 +929,7 @@ BEGIN
 					SELECT a.groupid, a.submitedin, a.approvedin FROM application a WHERE a.projectid = projectid AND YEAR(a.approvedin) != 0000;
 				ELSE
 					SELECT a.groupid, a.projectid, a.submitedin, a.approvedin FROM application a WHERE YEAR(a.approvedin) != 0000;
-				END IF;			
+				END IF;
 		END CASE;
 	END IF;
 END ;;
@@ -1086,6 +1092,29 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `queryToProc` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `queryToProc`(IN txt TEXT)
+BEGIN
+    SET @sql_txt = concat(txt);
+    INSERT INTO logProcExec VALUES (UUID(),txt,NOW());
+    PREPARE stmt FROM @sql_txt;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+  END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `updateUserAttribute` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -1098,7 +1127,7 @@ DELIMITER ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `updateUserAttribute`(IN userid INT, IN attributeid INT, IN `value` VARCHAR(255))
 BEGIN
-    UPDATE userattribute SET `value` = `value` WHERE userattribute.userid = userid AND userattribute.attributeid = attributeid; 
+    UPDATE userattribute SET `value` = `value` WHERE userattribute.userid = userid AND userattribute.attributeid = attributeid;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -1115,4 +1144,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-09 12:24:15
+-- Dump completed on 2017-06-09 18:11:04
